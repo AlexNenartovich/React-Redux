0cb52264b7a34f3f82dc7736f9344d7e12a15d9c
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2022-04-20 15:34:15 -0700

    Some changes

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import PostList from "./PostList";
 
 const Favorites = () => {
-
+  const favorites = useSelector((state) => state.posts.favorites);
   return (
-    <div></div>
-  )
-}
+    <div>
+      <PostList data={favorites} />
+    </div>
+  );
+};
 
 export default Favorites;
