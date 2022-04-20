87b926d50dbcfb66f11e6bfbdc287360174aec0e
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2022-04-20 11:57:13 -0700

    Some changes

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -29,6 +29,10 @@ export default (props) => {
   const res = posts.map((post) => (
     <Card className="post-list">
       <CardActionArea>
+        <img
+          style={{ height: 200, width: 300 }}
+          src="https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg"
+        />
         <h2>{post.title}</h2>
         <h3>{post.body}</h3>
       </CardActionArea>
