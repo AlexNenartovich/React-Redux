1fe064c6cb451f1ede7c8ceec44d5b6848738cbd
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2022-04-20 18:34:49 -0700

    Some changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import { useSelector } from "react-redux";
 import "./index.css";
+import Badge from "@material-ui/core/Badge";
+import MailIcon from "@material-ui/icons/Mail";
 
 const Header = () => {
+  const favCount = useSelector((state) => state.posts.favorites.length);
+
   return (
     <div className="container">
       <Button className="posts">
@@ -15,6 +20,9 @@ const Header = () => {
       <Button>
         <Link to="/favorites">Favorites</Link>
       </Button>
+      <Badge color="primary" badgeContent={favCount}>
+        <MailIcon />
+      </Badge>
     </div>
   );
 };
