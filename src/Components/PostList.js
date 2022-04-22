3b190d6475abc57c0782f8d1dd2c37f142a46184
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2022-04-21 20:43:57 -0700

    Some changes

--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default ({ data }) => {
-  console.log(data);
-  return data.map((post) => <Post post={post} />);
+  const [slice, setSlice] = useState(10);
+  const [more, setMore] = useState(true);
+  const incr = 10;
+  const len = data.length;
+
+  const handleClick = () => {
+    if (slice + incr > len) {
+      setMore(false);
+      return;
+    }
+    setTimeout(() => {
+      setSlice(slice + incr);
+    }, 2000);
+  };
+  const res = data.slice(0, slice).map((post) => <Post post={post} />);
+
+  return (
+    <InfiniteScroll
+      dataLength={slice}
+      next={handleClick}
+      hasMore={more}
+      loader={<h1>Loading...</h1>}
+      endMessage={<h2>You have reached the end</h2>}
+    >
+      <div>{res}</div>
+    </InfiniteScroll>
+  );
 };
