e2dbea284d35efc0d5c6a8dff2fb4717b31b2226
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2022-06-14 18:23:07 -0700

    Created edit

---
+++ src/Pages/edit.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Edit = () => {
+   
+  return (
+    <div>
+      <div>
+        <label>Title</label>
+      </div>
+      <div>
+        <input type="text"/>
+      </div>
+      <div>
+        <label>Body</label>
+      </div>
+      <div>
+        <textarea />
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </div>
+  )
+}
+
+export default Edit;
