fec89b12f2f6d647e1b57d3e0d8dcddfb4b603b0
AlexNenartovich <38087681+AlexNenartovich@users.noreply.github.com>
2022-08-17 18:04:29 -0700

    Added search functionality

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -5,11 +5,15 @@ import "./index.css";
 
 const Favorites = () => {
   const favorites = useSelector((state) => state.posts.favorites);
+  const search = useSelector((state) => state.posts.searchParam);
+  const resfav = favorites.filter((fav) =>
+    JSON.stringify(fav).includes(search)
+  );
   let res = "";
   if (favorites.length === 0) {
     res = <h2>You currently have no favorite posts</h2>;
   } else {
-    res = <PostList displayDelete={false} data={favorites} />;
+    res = <PostList displayDelete={false} data={resfav} />;
   }
   return <div className="post-container">{res}</div>;
 };
